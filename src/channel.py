79c492fc70eaed1db073509de7aa7e7eba33cf27
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-04-27 00:38:39 +0300

    Выполнение первой домашки.

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,8 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+api_key: str = os.getenv('YouTube_Data_API_v3')
 
 
 class Channel:
@@ -5,8 +10,10 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
