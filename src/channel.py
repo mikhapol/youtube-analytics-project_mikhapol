1d5a4979a4c487a44ddd2170a7c54ddd5f62f02f
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-04-27 01:09:05 +0300

    Выполнение второй домашки.

--- src/channel.py
+++ src/channel.py
@@ -10,10 +10,36 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        channel = Channel.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.url = channel["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+        self.subscriberCount = channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = channel["items"][0]["statistics"]["videoCount"]
+        self.viewCount = channel["items"][0]["statistics"]["viewCount"]
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self, file_json):
+        dump_dict = {"channel_id": self.__channel_id,
+                     "title": self.title,
+                     "description": self.description,
+                     "customUrl": self.url,
+                     "subscriberCount": self.subscriberCount,
+                     "video_count": self.video_count,
+                     "viewCount": self.viewCount}
+        with open(file_json, "w", encoding="utf-8") as file:
+            file.write(json.dumps(dump_dict, indent=2, ensure_ascii=False))
