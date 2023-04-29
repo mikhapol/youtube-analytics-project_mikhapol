e5d85f70ffe0fb45414b08f77d56696f16afc5b8
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-04-29 18:47:18 +0300

    Add homework-3

--- src/channel.py
+++ src/channel.py
@@ -19,6 +19,31 @@ class Channel:
         self.video_count = channel["items"][0]["statistics"]["videoCount"]
         self.viewCount = channel["items"][0]["statistics"]["viewCount"]
 
+    def __str__(self):
+        return f"{self.title} (https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA)"
+
+    def __add__(self, other):
+        return int(self.subscriberCount) + int(other.subscriberCount)
+
+    def __sub__(self, other):
+        return int(self.subscriberCount) - int(other.subscriberCount)
+
+    def __gt__(self, other):
+        return int(self.subscriberCount) > int(other.subscriberCount)
+
+    def __ge__(self, other):
+        return int(self.subscriberCount) >= int(other.subscriberCount)
+
+    def __lt__(self, other):
+        return int(self.subscriberCount) < int(other.subscriberCount)
+
+    def __le__(self, other):
+        return int(self.subscriberCount) <= int(other.subscriberCount)
+
+    def __eq__(self, other):
+        return int(self.subscriberCount) == int(other.subscriberCount)
+
+
     @property
     def channel_id(self):
         return self.__channel_id
