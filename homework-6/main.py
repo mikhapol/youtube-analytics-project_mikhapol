924b24e951384547403c01898e8e3bc00ab13707
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-05-17 22:34:36 +0300

    HM-6 финал

--- homework-6/main.py
+++ homework-6/main.py
@@ -8,3 +8,7 @@ if __name__ == '__main__':
     # broken_video_2 = Video('9lO06Zxhu88')
     # print(broken_video_2.title_video)
     # print(broken_video_2.like_count)
+    #
+    # broken_video_3 = Video('broken_video_id')
+    # print(broken_video_3.title_video)
+    # print(broken_video_3.like_count)
